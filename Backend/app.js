afcedfbace2eb44ea1cbce3391b5b2a3f1a70a5f
sunisathammasoon <5910110358@psu.ac.th>
2019-04-23 00:15:00 +0700

    create server

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+var socket = require('socket.io');
+socket = socket(server)
+
+socket.on('connection',(socket)=> {
+    console.log(socket.id);
+})
 
 server = app.listen(8080, function(){
     console.log('ser is runing on port 8080')
-    //test
+ 
 });
